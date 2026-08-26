3712f06ea57f3a94ffd4a69e8b122a11ffa727f0
abdelrahman <m1m2m3m4m5m6m700@gmail.com>
2026-08-26 20:16:12 +0300

    fix(router): register admin login route correctly

--- src/routes/route-tree.ts
+++ src/routes/route-tree.ts
@@ -30,7 +30,7 @@ import { imageToolRoutes } from './image-tools';
 import { indexRoute } from './index';
 import { localizedToolRoute } from './localized-tool';
 import { useCaseRoute } from './use-case';
-import { adminLoginRoute } from './admin-login';
+import { Route as adminLoginRoute } from './admin-login';
 import { Route as adminRoute } from './admin';
 
 export const routeChildren = [
