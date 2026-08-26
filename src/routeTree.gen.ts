122c404d94bf15f8cb73b2f5e6318c38e773f36e
abdelrahman <m1m2m3m4m5m6m700@gmail.com>
2026-08-26 22:04:55 +0300

    fix(router): remove temporary Start generated tree

--- src/routeTree.gen.ts
+++
@@ -1,6 +0,0 @@
-/* eslint-disable */
-// Compatibility placeholder for typechecking; TanStack Start regenerates this
-// route tree from src/start-routes at build/dev time.
-import { Route } from './start-routes/__root';
-
-export const routeTree = Route;
