53c96cfca0de73054bc75846c680d7f11c1e9514
abdelrahman <m1m2m3m4m5m6m700@gmail.com>
2026-08-26 22:05:17 +0300

    fix(router): remove temporary Start catch-all route

--- src/start-routes/$.tsx
+++
@@ -1,6 +0,0 @@
-import { createFileRoute } from '@tanstack/react-router';
-import { StartAppBridge } from '../start-app-bridge';
-
-export const Route = createFileRoute('/$')({
-  component: StartAppBridge,
-});
