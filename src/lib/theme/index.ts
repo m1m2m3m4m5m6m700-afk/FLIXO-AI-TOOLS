fc1f1a90f97d89172816697729428c984daa1c4d
abdelrahman <m1m2m3m4m5m6m700@gmail.com>
2026-09-05 05:17:45 +0300

    refactor(theme): expose persistent theme surface

---
+++ src/lib/theme/index.ts
@@ -0,0 +1,10 @@
+export {
+  THEME_STORAGE_KEY,
+  applyTheme,
+  installPersistentTheme,
+  normalizeThemePreference,
+  persistTheme,
+  readStoredTheme,
+  resolveTheme,
+} from './persistent-theme';
+export type { ResolvedTheme, ThemePreference } from './persistent-theme';
